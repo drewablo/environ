9dc43af150d0b50815094791645e78eaf31ec5c3
drewablo <andrewt77@gmail.com>
2020-02-17 15:41:09 -0600

    Update environ.py

--- environ.py
+++ environ.py
@@ -22,9 +22,7 @@ sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 sgp30.iaq_init()
 sgp30.set_iaq_baseline(0x8973, 0x8aae)
 
-dht = adafruit_dht.DHT11(board.D3)
-temperature = dht.temperature
-humidity = dht.humidity
+dht = adafruit_dht.DHT11(board.D4)
 
 display = board.DISPLAY
 splash = displayio.Group(max_size=75)
@@ -71,60 +69,56 @@ def translate(val, OldMin, OldMax, NewMin, NewMax):
 def setBacklight(val):
     val = max(0, min(1.0, val))
     board.DISPLAY.auto_brightness = False
-    board.DISPLAY.brightness = val 
+    board.DISPLAY.brightness = val
+    
 
-def textUpdate(sensorName):
-	splash.pop()
-	splash.pop()
-	if sensorName == eCO2:
-		textDisplay(str(sgp30.eCO2), font, firstRowX, firstRowY-10, 0x000000, splash)
-	elif sensorName == TVOC:
-		textDisplay(str(sgp30.TVOC), font, firstRowX+2*r+10, firstRowY-10, 0x000000, splash)
-	elif sensorName == temp:
-		textDisplay(str(sgp30.eCO2), font, secondRowX, secondRowX-10, 0x000000, splash)
-	elif sensorName == humid:
-		textDisplay(str(sgp30.TVOC), font, secondRowX+2*r+10, secondRowY-10, 0x000000, splash)	
-		
-def gaugeDraw(sensorName, newVal, oldVal, r, w, gaugeCenterX, gaugeCenterY, color, setUp = False):
-	totalDegrees = newVal - oldVal
-	if newVal == oldVal:
-		pass
-	elif newVal > oldVal:
-		if setUP == False:
-			textUpdate(sensorName)
-		for i in range(r):
-			x1 = round(cos(totalDegrees - i) * r) + gaugeCenterX
-			x2 = round(cos(totalDegrees - i) * w) + gaugeCenterX
-			y = gaugeCenterY + i
-			for q in range(x1, x2):
-				x = x1+q
-				gaugeBmp[x,y] = color
-	elif newVal > oldVal:
-		if setUP == False:
-			textUpdate(sensorName)
-		for a in range(r):
-				x1 = round(cos(totalDegrees - a) * r) + gaugeCenterX
-				x2 = round(cos(totalDegrees - a) * w) + gaugeCenterX
-				y = gaugeCenterY + i
-				for b in range(x1, x2):
-					x = x1+b
-					gaugeBmp[x,y] = color
+def gaugeDraw(newVal, oldVal, r, w, gaugeCenterX, gaugeCenterY, color, setUP = False):
+    if newVal == oldVal:
+        pass
+    elif newVal > oldVal:
+        if setUP == False:
+            splash.pop()
+            splash.pop()
+            textDisplay(str(sgp30.eCO2), font, firstRowX, firstRowY-10, 0x000000, splash)
+            textDisplay(str(sgp30.TVOC), font, firstRowX+2*r+10, firstRowY-10, 0x000000, splash)
+        for i in range(oldVal, newVal):
+            outerX = round(cos(radians(i * coveragePercent)) * r) + gaugeCenterX
+            outerY = round(sin(radians(i * coveragePercent)) * r) + gaugeCenterY
+            gaugeBmp[outerX,outerY] = color
+            for q in range(w):
+                x = round(cos(radians(i * coveragePercent)) * (r-q)) + gaugeCenterX
+                y = round(sin(radians(i * coveragePercent)) * (r-q)) + gaugeCenterY
+                gaugeBmp[x,y] = color
+    elif newVal < oldVal:
+        if setUP == False:
+            splash.pop()
+            splash.pop()
+            textDisplay(str(sgp30.eCO2), font, firstRowX, firstRowY-10, 0x000000, splash)
+            textDisplay(str(sgp30.TVOC), font, firstRowX+2*r+10, firstRowY-10, 0x000000, splash)
+        for a in range(oldVal, newVal, -1):
+            outerX = round(cos(radians(a * coveragePercent)) * r) + gaugeCenterX
+            outerY = round(sin(radians(a * coveragePercent)) * r) + gaugeCenterY
+            gaugeBmp[outerX,outerY] = 3
+            for b in range(w):
+                x = round(cos(radians(a * coveragePercent)) * (r - b)) + gaugeCenterX
+                y = round(sin(radians(a * coveragePercent)) * (r - b)) + gaugeCenterY
+                gaugeBmp[x,y] = 3
 
-
+coveragePercent = .7
 firstRowX = 130
-firstRowY = 250
+firstRowY = 275
 secondRowX = 130
-firstRowY = 130
-gaugeMax = 315
-gaugeMin = 180
+secondRowy = 125
+gaugeMax = int(315/coveragePercent)
+gaugeMin = int(180/coveragePercent)
 eCO2Data = translate(sgp30.eCO2, 400, 60000, gaugeMin, gaugeMax)
 tvocData = translate(sgp30.TVOC, 0, 20000, gaugeMin, gaugeMax)
-tempData = translate(temperature, -10, 50, gaugeMin, gaugeMax)
-humidData = translate(humidity, 0, 100, gaugeMin, gaugeMax)
+tempData = translate(dht.temperature, -10, 50, gaugeMin, gaugeMax)
+humidData = translate(dht.humidity, 0, 100, gaugeMin, gaugeMax)
 eCO2Previous = eCO2Data
 tvocPrevious = tvocData
-tempPrevious = temperature
-humidPrevious = humidity
+tempPrevious = tempData
+humidPrevious = humidData
 setBacklight(0.75)
 
 rect1a = Rect(firstRowX-r-4, firstRowY, r*2-10, 2, fill=0x000000)
@@ -135,54 +129,50 @@ splash.append(rect1a)
 splash.append(rect1b)
 splash.append(rect2a)
 splash.append(rect2b)
-rect3a = Rect(secondRowX-r-4, secondRowY, r*2-10, 2, fill=0x000000)
-rect3b = Rect(secondRowX+r-15, secondRowY-r, 8, r+2, fill=0x000000)
-rect4a = Rect(secondRowX+r*2+6-r, secondRowY, r*2-10, 2, fill=0x000000)
-rect5b = Rect(secondRowX+3*r-5, secondRowY-r, 8, r+2, fill=0x000000)
+rect3a = Rect(secondRowX-r-4, secondRowy, r*2-10, 2, fill=0x000000)
+rect3b = Rect(secondRowX+r-15, secondRowy-r, 8, r+2, fill=0x000000)
+rect4a = Rect(secondRowX+r*2+6-r, secondRowy, r*2-10, 2, fill=0x000000)
+rect4b = Rect(secondRowX+3*r-5, secondRowy-r, 8, r+2, fill=0x000000)
 splash.append(rect3a)
 splash.append(rect3b)
 splash.append(rect4a)
 splash.append(rect4b)
-
 for i in range(2):
     x = (firstRowX + ((r*2+10)*i))
     gaugeDraw(gaugeMax, gaugeMin, r+4, w+4, x, firstRowY, 3, True)
-    gaugeDraw(317/coveragePercent, 315, r+15, w+30, x, firstRowY, 3, True)
-for j in range(2):
-    x = (secondRowX + ((r*2+10)*j))
-    gaugeDraw(gaugeMax, gaugeMin, r+4, w+4, x, secondRowY, 3, True)
-    gaugeDraw(317/coveragePercent, 315, r+15, w+30, x, secondRowY, 3, True)
-for k in range(2):
-    x = (firstRowX + ((r*2+10)*k))
+    gaugeDraw(317/coveragePercent, 315/coveragePercent, r+15, w+30, x, firstRowY, 3, True)
+for i in range(2):
+    x = (secondRowX + ((r*2+10)*i))
+    gaugeDraw(gaugeMax, gaugeMin, r+4, w+4, x, secondRowy, 3, True)
+    gaugeDraw(317/coveragePercent, 315/coveragePercent, r+15, w+30, x, secondRowy, 3, True)    
+for q in range(2):
+    x = (firstRowX + ((r*2+10)*q))
     gaugeDraw(gaugeMax,gaugeMin, r, w-4, x, firstRowY, 1, True)
     gaugeDraw(gaugeMin,gaugeMax, r, w-4, x, firstRowY, 1, True)
-for l in range(2):
-    x = (secondRowX + ((r*2+10)*l))
-    gaugeDraw(gaugeMax,gaugeMin, r, w-4, x, secondRowY, 1, True)
-    gaugeDraw(gaugeMin,gaugeMax, r, w-4, x, secondRowY, 1, True)
-	
+sleep(2)
+gaugeDraw(tempData, gaugeMin, r, w-4, secondRowX, secondRowy, 1, True)
+gaugeDraw(humidData, gaugeMin, r, w-4, (secondRowX + (r*2+10)), secondRowy, 1, True)
+
 textDisplay("eCO2", font, firstRowX-r, firstRowY+20, 0x000000, splash)
 textDisplay("TVOC", font, firstRowX+10+r, firstRowY+20, 0x000000, splash)
-condition = Rect(firstRowX-r-4, 20, 160, 70, fill=0x000000)
-condition2 = Rect(firstRowX+60, 20, 275, 70, fill=0x000000)
-splash.append(condition)
-splash.append(condition2)
 textDisplay(str(sgp30.eCO2), font, firstRowX, firstRowY-10, 0x000000, splash)
 textDisplay(str(sgp30.TVOC), font, firstRowX+2*r+10, firstRowY-10, 0x000000, splash)
+print(str(len(splash)))
 
 while True:
     print("eCO2: " + str(sgp30.eCO2))
     print("tvoc: " + str(sgp30.TVOC))
     eCO2Data = translate(sgp30.eCO2, 400, 60000, gaugeMin, gaugeMax)
     tvocData = translate(sgp30.TVOC, 0, 20000, gaugeMin, gaugeMax)
-	tempData = translate(temperature, -10, 50, gaugeMin, gaugeMax)
-	humidData = translate(humidity, 0, 100, gaugeMin, gaugeMax)
-    gaugeDraw("eCO2", eCO2Data, eCO2Previous, r, w-4, firstRowX, firstRowY, 1)
-    gaugeDraw("TVOC", tvocData, tvocPrevious, r, w-4, (firstRowX+(r*2+10)), firstRowY, 1)
-	gaugeDraw("temp", tempData, tempPrevious, r, w-4, secondRowX, secondRowY, 1)
-	gaugeDraw("humid", humidData, humidPrevious, r, w-4, (secondRowX+(r*2+10)), secondRowY, 1)
+    tempData = translate(dht.temperature, -10, 50, gaugeMin, gaugeMax)
+    humidData = translate(dht.humidity, 0, 100, gaugeMin, gaugeMax)
+    gaugeDraw(eCO2Data, eCO2Previous, r, w-4, firstRowX, firstRowY, 1)
+    gaugeDraw(tvocData, tvocPrevious, r, w-4, (firstRowX+(r*2+10)), firstRowY, 1)
+    gaugeDraw(tempData, tempPrevious, r, w-4, secondRowX, secondRowy, 1)
+    gaugeDraw(humidData, humidPrevious, r, w-4, (secondRowX+(r*2+10)), secondRowy, 1)
     sleep(.5)
+    print(str(len(splash)))
     eCO2Previous = eCO2Data
     tvocPrevious = tvocData
-	tempPrevious = tempData
-	humidPrevious = humidData
+    tempPrevious = tempData
+    humidPrevious = humidData
