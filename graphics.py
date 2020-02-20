1ec146871c46a1b4a059e2610476f3e1b9f2a644
drewablo <andrewt77@gmail.com>
2020-02-20 15:54:34 -0600

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,99 +1,205 @@
-import board
-import displayio
-import busio
-from math import cos
-from math import sin
-from math import asin
-from math import radians
-from time import time
-from time import sleep
+from math import *
+from graphics import *
+from time import *
 from random import randint
 
-display = board.DISPLAY
-splash = displayio.Group(max_size=50)
+win = GraphWin("TITANO",480,320)
 
-# Background
-BGbitmap = displayio.Bitmap(display.width, display.height, 1)
-BGpalette = displayio.Palette(1)
-BGpalette[0] = 0x50b0a8
-BGsprite = displayio.TileGrid(BGbitmap, x=0, y=0, pixel_shader=BGpalette)
-splash.append(BGsprite)
+	   
+yCenter = 200
+xCenter = 300
+newVal = 360	  
+oldVal = 180
+r = 70
+w = 50
+arcWidth = r - w
 
-# Palette for gauge bitmap
-palette = displayio.Palette(4)
-palette[0] = 0x000000
-palette[1] = 0x97f3e8 #lightturq
-palette[2] = 0x50b0a8 #medturk
-palette[3] - 0x408890 #darkturq
-palette.make_transparent(0)
+def line(color, x0, y0, x1, y1, *args, **kwargs):
+		steep = abs(y1 - y0) > abs(x1 - x0)
+		if steep:
+			x0, y0 = y0, x0
+			x1, y1 = y1, x1
+		if x0 > x1:
+			x0, x1 = x1, x0
+			y0, y1 = y1, y0
+		dx = x1 - x0
+		dy = abs(y1 - y0)
+		err = dx // 2
+		ystep = 0
+		if y0 < y1:
+			ystep = 1
+		else:
+			ystep = -1
+		while x0 <= x1:
+			if steep:
+				pt = Point(y0,x0,*args,**kwargs)
+				pt.setFill(color)
+				pt.draw(win)
 
-# Create gauge bitmap
-gaugeBmp = displayio.Bitmap(display.width, display.height, len(palette))
-gauge = displayio.TileGrid(gaugeBmp, pixel_shader=palette)
-splash.append(gauge)
+			else:
+				pt = Point(x0,y0,*args,**kwargs)
+				pt.setFill(color)
+				pt.draw(win)
 
-# show splash group
-display.show(splash)
+			err -= dy
+			if err < 0:
+				y0 += ystep
+				err += dx
+			x0 += 1
+			
+def arc(oldVal, newVal, r, w, yCenter, xCenter, color):
+	outerYmax = round(sin(radians(newVal)) * r) + yCenter
+	outerYmin = round(sin(radians(oldVal)) * r) + yCenter
+	outerXmax = round(cos(radians(newVal)) * r ) + xCenter
+	outerYcenter = round(sin(radians(270)) * r) + yCenter
+	
+	innerYmax = round(sin(radians(newVal)) * w) + yCenter
+	innerXmax = round(cos(radians(newVal)) * w ) + xCenter
+	innerYcenter = round(sin(radians(270)) * w) + yCenter
+	innerYmin = round(sin(radians(newVal)) * w) + yCenter #past 270 degrees
+	
+	if newVal == oldVal:
+		pass
+	elif newVal > oldVal:
+		if newVal > 270:
+			innerYmax = innerYcenter
+			outerYmax = outerYcenter
+		# draw right arc
+		for y in range(outerYmin, outerYmax, -1):
+			print("outerYmax: " +str(outerYmax))
+			print("outerYmin: " +str(outerYmin))
+			print("innerYmax: " +str(innerYmax))
+			print("Y1: " + str(y))
+			if y > innerYmax:
+				theta1 = asin((yCenter - y)/r)
+				theta2 = asin((yCenter - y)/w)
+				x1 = round(cos((theta1)) * r) + xCenter
+				x2 = round(cos((theta2)) * w) + xCenter
+				for x in range(x2, x1):
+					pt = Point(x,y)
+					pt.setFill(color)
+					pt.draw(win)
+				print("x1: " + str(x1))
+				print("x2: " + str(x2))
+			elif newVal > 270 and y < innerYmax:
+				print("MIDDLE....................")
+				theta1 = asin((yCenter - y)/r)
+				x1 = round(cos((theta1)) * r) + xCenter
+				print(x1)
+				for x in range(x1, xCenter, -1):
+					pt = Point(x,y)
+					pt.setFill(color)
+					pt.draw(win)
+			elif innerYmax - outerYmax <= arcWidth: #this gives it a angle on the end of the arc instead of a horizontal line
+				w += 1.5
+				theta1 = asin((yCenter - y)/r)
+				theta2 = asin((yCenter - y)/w)
+				print("W: " + str(w))
+				x1 = round(cos((theta1)) * r) + xCenter
+				x2 = round(cos((theta2)) * w) + xCenter
+				for x in range(x2, x1):
+					pt = Point(x,y)
+					pt.setFill(color)
+					pt.draw(win)
+				print("x1: " + str(x1))
+				print("x2: " + str(x2))
+		# Draw left arc
+		if 270 < newVal <= 360:
+			outerYmax = round(sin(radians(newVal)) * r ) + yCenter
+			print("OUTERyMAX: " + str(outerYmax))
+			print("INNERyMIN: " + str(innerYmin))
+			for y in range(outerYcenter, innerYmin):
+				print("Y: " + str(y))
+				if y <= innerYcenter:
+					theta1 = asin((yCenter - y)/r)
+					x1 = xCenter - round(cos((theta1)) * r)
+					for x in range(xCenter, x1, -1):
+						pt = Point(x,y)
+						pt.setFill(color)
+						pt.draw(win)
+				elif innerYcenter < y <= outerYmax:
+					print("SWITCH")
+					theta1 = asin((yCenter - y)/r)
+					theta2 = asin((yCenter - y)/w)
+					x1 = xCenter - round(cos(theta1) * r)
+					x2 = xCenter - round(cos(theta2) * w)
+					for x in range(x2, x1, -1):
+						pt = Point(x,y)
+						pt.setFill(color)
+						pt.draw(win)
+				elif outerYmax - innerYmin	<= arcWidth: 
+					r -= 1.5
+					theta1 = asin((yCenter - y)/r)
+					theta2 = asin((yCenter - y)/w)
+					x1 = xCenter - round(cos(theta1) * r)
+					x2 = xCenter - round(cos(theta2) * w)
+					print("x1: " +str(x1))
+					print("x2: " +str(x2))
+					for x in range(x2, x1, -1):
+						pt = Point(x,y)
+						pt.setFill(color)
+						pt.draw(win)
 
-r = 70 #outer gauge radius
-iRad = 20 #inner gauge radius
+		
+#arc(oldVal, newVal, r+5, w-5, yCenter, xCenter, "blue")
 
-def gaugeDraw(newVal, oldVal, r, iRad, gaugeCenterX, gaugeCenterY, color, setUp = False):
-    y = gaugeCenterY
-    if newVal == oldVal:
-        pass
-    elif newVal > oldVal:
-        totalDegrees = newVal - oldVal
-        print(totalDegrees)
-        sleep(1)
-        for i in range(180, totalDegrees):
-            print("BIGGER I: " + str(i))
-            y = y+1
-            x1 = int(cos(radians(i)) * r) + gaugeCenterX#outter x
-            print("x1: " + str(x1))
-            x2 = int(cos(asin(radians(y/iRad)))*iRad) + gaugeCenterX
-            #print("x2: " + str(x2))
-            vLineRange = x2 - x1
-            #print("range " + str(vLineRange))
-            sleep(1)
-            for q in range(vLineRange):
-                x = x1 + q
-                gaugeBmp[x,y] = color
-    elif newVal < oldVal:
-        maxY = int(sin(radians(oldVal)) * r) + gaugeCenterY
-        minY = int(sin(radians(newVal)) * r) + gaugeCenterY
-        totalDegrees = oldVal - newVal
-        print(totalDegrees)
-        for i in range(maxY, minY, -1):
-            print("I: " + str(i))
-            y = y-1
-            x1 = int(cos(radians(i)) * r) + gaugeCenterX
-            print("x1: " + str(x1))
-            a2 = int(asin(radians(y/iRad)))
-            a2 = a2 - i
-            x2 = int(cos(asin(radians(y/iRad)))*iRad) + gaugeCenterX
-            print("x2: " + str(x2))
-            vLineRange = abs(x1 - x2)
-            print("Vertical Line Lenght: " +str(vLineRange))
-            for q in range(vLineRange):
-                x = x1 + q
-                gaugeBmp[x,y] = 3
- 
-firstRowX = 95
-firstRowY = 95
+def arc(newVal, oldVal, outer, inner, color):
+	if newVal == oldVal:
+		pass
+	elif newVal > oldVal:
+		for i in range(newVal,oldVal,-1):
+				y = round(sin(radians(i)) * outer) + yCenter
+				x = round(cos(radians(i)) * outer) + xCenter
+				y2 = round(sin(radians(i)) * inner) + yCenter
+				x2 = round(cos(radians(i)) * inner) + xCenter
+				line("red", x2, y2, x, y)
+	elif newVal < oldVal:
+		for i in range(oldVal, newVal):
+				y = round(sin(radians(i)) * outer) + yCenter
+				x = round(cos(radians(i)) * outer) + xCenter
+				y2 = round(sin(radians(i)) * inner) + yCenter
+				x2 = round(cos(radians(i)) * inner) + xCenter
+				line("red", x2, y2, x, y)
+def line(color, x0, y0, x1, y1, *args, **kwargs):
+		steep = abs(y1 - y0) > abs(x1 - x0)
+		if steep:
+			x0, y0 = y0, x0
+			x1, y1 = y1, x1
+		if x0 > x1:
+			x0, x1 = x1, x0
+			y0, y1 = y1, y0
+		dx = x1 - x0
+		dy = abs(y1 - y0)
+		err = dx // 2
+		ystep = 0
+		if y0 < y1:
+			ystep = 1
+		else:
+			ystep = -1
+		while x0 <= x1:
+			if steep:
+				pt = Point(y0,x0,*args,**kwargs)
+				pt.setFill(color)
+				pt.draw(win)
 
-rando2 = randint(179, 315)
-rando3 = randint(180, 315)
-rando4 = randint(189, 315)
+			else:
+				pt = Point(x0,y0,*args,**kwargs)
+				pt.setFill(color)
+				pt.draw(win)
 
-for i in range(3):
-    x = (firstRowX + (r*2*i))
-    gaugeDraw(180, 315, r+4, iRad+4, x, firstRowY, 3)
-
+			err -= dy
+			if err < 0:
+				y0 += ystep
+				err += dx
+			x0 += 1
+			
+yCenter = 200
+xCenter = 300
+r = 110
+w = 90
+r1 = 80
+w1 = 40
 while True:
-    rando1 = randint(179, 315)
-    for i in range(3):
-        x = (firstRowX + (r*2*i))
-        gaugeDraw(rando1, rando2, r, iRad, x, firstRowY, 1)
-        rando2 = rando1
-    sleep(1)
+	arc(newVal, oldVal, r, w, "purple")
+	sleep(3)
+	break
